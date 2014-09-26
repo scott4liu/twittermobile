b7a180d211e5a3e896f92f6aa0d5cac1ab75cc0b
Scott Liu <scott.s.liu@gmail.com>
2014-09-26 15:56:06 -0700

    Initial Commit

---
+++ TwitterMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterMobile
+//
+//  Created by Scott Liu on 9/26/14.
+//  Copyright (c) 2014 Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
